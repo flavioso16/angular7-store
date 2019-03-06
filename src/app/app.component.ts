944196f296072b37ba79aa5306178d36489f6684
Flavio Oliviera <3rd.flavio.santos@almundo.com>
2019-03-05 23:30:53 -0300

    including configurations of routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe';
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
   }
 
 }
